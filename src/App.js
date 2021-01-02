79495d40a0a40a06d3fd0ab3882214bd4e42384f
Miikolaj Andrzejewski <mikandrzej12321@gmail.com>
2021-01-02 21:43:47 +0100

    add router

--- src/App.js
+++ src/App.js
@@ -1,18 +1,23 @@
 import "./App.css";
 import Header from "./Header";
 import Home from "./Home";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <h1> hello ! 🚀</h1>
-
-      {/* Header */}
-      <Header />
-
-      {/* Home */}
-      <Home />
-    </div>
+    <Router>
+      <div className="App">
+        <Header />
+        <Switch>
+          <Route path="/checkout">
+            <h1>I am the checkout, smash the like button</h1>
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
