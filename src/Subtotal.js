b8cb868fc3571592094beb5968899e015b70bd46
Miikolaj Andrzejewski <mikandrzej12321@gmail.com>
2021-01-03 00:29:40 +0100

    prepare data context

--- src/Subtotal.js
+++ src/Subtotal.js
@@ -1,8 +1,32 @@
 import React from "react";
 import "./Subtotal.css";
+import CurrencyFormat from "react-currency-format";
 
 function Subtotal() {
-  return <div className="subtotal"></div>;
+  return (
+    <div className="subtotal">
+      <CurrencyFormat
+        renderText={(value) => (
+          <>
+            <p>
+              Subtotal (0 items): <strong>{value}</strong>
+            </p>
+            <small className="subtotal__gift">
+              <input type="checkbox" />
+              This order contains a gift
+            </small>
+          </>
+        )}
+        decimalScale={2}
+        value={1.23}
+        displayType={"text"}
+        thousandsSeparator={true}
+        suffix={" PLN"}
+      />
+
+      <button>Proceed to Checkout</button>
+    </div>
+  );
 }
 
 export default Subtotal;
