436bb16aec01fd78d1643817327e98e1b00c153d
Miikolaj Andrzejewski <mikandrzej12321@gmail.com>
2021-01-06 16:42:59 +0100

    change functions api to public

--- src/axios.js
+++ src/axios.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: `http://localhost:5001/clone-d33ee/us-central1/api`,
+  // baseURL: `http://localhost:5001/clone-d33ee/us-central1/api`,
+  baseURL: `https://us-central1-clone-d33ee.cloudfunctions.net/api`,
 });
 
 export default instance;
